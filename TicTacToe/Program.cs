46f91ccc309777afa75884124355973459abfc59
Pascal <pascal.stein1989@gmail.com>
2025-05-01 21:26:46 +0200

    added player inputs and draw conditions

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Xml.Serialization;
 
 namespace TicTacToe
@@ -70,12 +71,14 @@ namespace TicTacToe
                     for (int col = 0; col < 3; col++)
                     {
                         Console.Write($" {gameboard[row, col]} ");
+                        // if col is below 2, add a | after the number, so after 1 and 2 but not after 3 which gives us 1 | 2 | 3
                         if (col < 2)
                         {
                             Console.Write("|");
                         }
                     }
                     Console.WriteLine("");
+                    // if row is below 2, add "-----------" after the row, so after 1 | 2 | 3 and 4 | 5 | 6, but not after 7 | 8 | 9
                     if (row < 2)
                     {
                         Console.WriteLine("-----------");
@@ -121,23 +124,71 @@ namespace TicTacToe
             // checking row win conditions
             static bool CheckRow(int row, char player)
             {
+                // checking for 1 | 2 | 3 and 4 | 5 | 6 and  7 | 8 | 9
                 return gameboard[row, 0] == player && gameboard[row, 1] == player && gameboard[row, 2] == player;
             }
 
             // checking col win conditions
             static bool CheckCol(int col, char player)
             {
+                // checking 1 | 4 | 7 and 2 | 5 | 8 and  3 | 6 | 9
                 return gameboard[0, col] == player && gameboard[1, col] == player && gameboard[2, col] == player;
             }
 
+            // check for a draw
             static bool CheckDraw()
             {
-                throw new NotImplementedException();
+                if (CheckWinCondition() == false) // if the game isn't won / over yet
+                {
+                    for (int row = 0; row < 3; row++) // for each row (there are 3)
+                    {
+                        for (int col = 0; col < 3; col++) // for each col (there are 3)
+                        {
+                            if (gameboard[row, col] != 'X' && gameboard[row, col] != 'O') // check if the board has all X or O, if no, return false, if yes true is returned
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+                return true;
             }
 
             static void GetPlayerMove()
             {
-                throw new NotImplementedException();
+                int move;
+                bool isValidMove = false;
+
+                // do while runs atleast once, regardless of the condition
+                do
+                {
+                    Console.Write($"Player {currentPlayer}, choose a number between 1-9: ");
+                    isValidMove = int.TryParse(Console.ReadLine(), out move) && move >= 1 && move <= 9;  // TryParse checks if the input is an integer, if not if return false and if it is, its saved in the variable move
+
+                    if (isValidMove)
+                    {
+                        // convert int to indicies
+                        // checks for the dividend, which will be the row
+                        int row = (move - 1) / 3;
+                        // checks for the remainder of the division, which will be the col
+                        int col = (move - 1) % 3;
+
+                        if (gameboard[row, col] == currentPlayer)
+                        {
+                            Console.WriteLine($"DThe field is already occupied with {gameboard[row, col]}.");
+                            
+                        }
+                        else
+                        {
+                            gameboard[row, col] = currentPlayer;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid move, please enter a correct number between 1-9");
+                    }
+                
+                } while (!isValidMove);   
             }
         }
     }
